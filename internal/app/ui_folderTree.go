436f8cb19ffeda25440fa395a4fffc9a192f3e2e
Vladislav Gudov <vlad.gudov@inbox.ru>
2022-02-20 16:21:54 +0400

    node struct for tree

--- internal/app/ui_folderTree.go
+++ internal/app/ui_folderTree.go
@@ -1,8 +1,16 @@
 package app
 
 import (
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
+
+type node struct {
+	text     string
+	expand   bool
+	selected func()
+	children []*node
+}
 
 func (ui *UI) folderTree(nextSlide func()) (string, *tview.Primitive) {
 	input := tview.NewInputField().
@@ -11,5 +19,25 @@ func (ui *UI) folderTree(nextSlide func()) (string, *tview.Primitive) {
 	input.SetText("") // обновлять директории по кликам в дереве
 	// })
 	// tree = tview.NewTreeView()
-	return "", nil
+	return "Директория", nil
+}
+
+func add(target *node) *tview.TreeNode {
+	node := tview.NewTreeNode(target.text).
+		SetSelectable(target.expand || target.selected != nil).
+		SetExpanded(true).
+		SetReference(target)
+	if target.expand {
+		node.SetColor(tcell.ColorGreen)
+	} else if target.selected != nil {
+		node.SetColor(tcell.ColorRed)
+	}
+	for _, child := range target.children {
+		node.AddChild(add(child))
+	}
+	return node
+}
+
+func (ui *UI) setTreeDir(path string) {
+
 }
